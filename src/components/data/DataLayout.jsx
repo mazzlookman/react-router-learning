580f5a3ebabbf8f6d7229d76eca28fe448813d49
Mohammad Lukman Aqib <mohammadlukmanaqib@gmail.com>
2025-07-30 14:21:44 +0700

    navigation: link-to

--- src/components/data/DataLayout.jsx
+++ src/components/data/DataLayout.jsx
@@ -1,10 +1,23 @@
-import {Outlet} from "react-router";
+import {Link, Outlet} from "react-router";
 
 export default function DataLayout() {
     return (
         <>
             <div>
                 <h1>Header Data</h1>
+            </div>
+            <div>
+                <ul style={{display: 'flex', listStyle: 'none', gap: '1rem'}}>
+                    <li><Link to={'/data/product'}>Products</Link></li>
+                    <li><Link to={{
+                        pathname: '/data/product',
+                        search: '?id=123&murce=true',
+                        hash: '#top',
+                    }}>Products with query</Link></li>
+                    <li><Link to={'/data/customer'}>Customers</Link></li>
+                    <li><Link to={'/data/seller'}>Sellers</Link></li>
+                    <li><a href="/data/seller">Reload sellers</a></li>
+                </ul>
             </div>
             <div>
                 <Outlet />
